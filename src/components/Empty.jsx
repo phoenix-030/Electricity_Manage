ac88b32ad372751d8578ef26f64dfc9907982a44
phoenix-030 <harishkumar28950@gmail.com>
2026-02-01 03:20:03 +0530

    Fix minor code issues

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom'
-import "./assets/App.css";
+import "../styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 const Empty = () => {
   return (
-<>
-   
-
-</>
+    <>
+      <div className="container text-center mt-5">
+        <h2>Page Not Found</h2>
+        <p>The page you're looking for doesn't exist.</p>
+        <Link to="/" className="btn btn-primary">Go Home</Link>
+      </div>
+    </>
   );
 };
 
